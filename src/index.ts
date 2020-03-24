5eda7c9f83e8d58e31ff412e390bd6abd55b7ada
Reji Dasan <reji@k2.com>
2020-03-24 09:33:09 -0700

    adding async and promise support

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ metadata = {
     description: "An sample broker that accesses OData."
 };
 
-ondescribe = function() {
+ondescribe = async function (): Promise<void> {
     postSchema({
         // https://www.odata.org/getting-started/understand-odata-in-6-steps/
         // http://olingo.apache.org/doc/javascript/index.html
@@ -17,18 +17,18 @@ ondescribe = function() {
                 properties: {
                     "com.sample.odata.people.userName": {
                         displayName: "User Name",
-                        type: "string" 
+                        type: "string"
                     },
                     "com.sample.odata.people.city": {
                         displayName: "City",
-                        type: "string" 
+                        type: "string"
                     }
                 },
                 methods: {
                     "com.sample.odata.people.get": {
                         displayName: "Get People",
                         type: "read",
-                        outputs: [ "com.sample.odata.people.userName", "com.sample.odata.people.city" ]
+                        outputs: ["com.sample.odata.people.userName", "com.sample.odata.people.city"]
                     }
                 }
             }
@@ -36,42 +36,48 @@ ondescribe = function() {
     });
 }
 
-onexecute = function(objectName, methodName, parameters, properties) {
-    switch (objectName)
-    {
+onexecute = async function (objectName, methodName, parameters, properties): Promise<void> {
+    switch (objectName) {
         case "com.sample.odata.people": onexecuteGet(methodName, parameters, properties); break;
         default: throw new Error("The object " + objectName + " is not supported.");
     }
 }
 
-function onexecuteGet(methodName: string, parameters: SingleRecord, properties: SingleRecord) {
-    switch (methodName)
-    {
+async function onexecuteGet(methodName: string, parameters: SingleRecord, properties: SingleRecord): Promise<void> {
+    switch (methodName) {
         case "com.sample.odata.people.get": onexecuteGetPeople(parameters, properties); break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
-function onexecuteGetPeople(parameters: SingleRecord, properties: SingleRecord) {
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+function onexecuteGetPeople(parameters: SingleRecord, properties: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+        xhr.onreadystatechange = function () {
+            try {
+                if (xhr.readyState !== 4) return;
+                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
 
-        console.log(xhr.responseText);
-        var obj = JSON.parse(xhr.responseText);
-        console.log(obj.AddressInfo[0].City.Name); 
+                console.log(xhr.responseText);
+                var obj = JSON.parse(xhr.responseText);
+                console.log(obj.AddressInfo[0].City.Name);
 
-        postResult({
-            "com.sample.odata.people.userName": obj.UserName,
-            "com.sample.odata.people.city": obj.AddressInfo[0].City.Name
-        });
-    };
+                postResult({
+                    "com.sample.odata.people.userName": obj.UserName,
+                    "com.sample.odata.people.city": obj.AddressInfo[0].City.Name
+                });
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
 
-    var url = "https://services.odata.org/V4/(S(a2k31bgwiyejn2j2iiybvq4p))/TripPinServiceRW/People('russellwhyte')";
-    xhr.open("GET", url);
-   
-    xhr.send();
+        };
+
+        var url = "https://services.odata.org/V4/(S(a2k31bgwiyejn2j2iiybvq4p))/TripPinServiceRW/People('russellwhyte')";
+        xhr.open("GET", url);
+
+        xhr.send();
+    });
 }
